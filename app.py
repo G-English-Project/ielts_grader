6d8d249259bc28289c9e7e480901bd90ea2efe8c
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2026-04-21 00:03:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -979,4 +979,4 @@ def post_gdoc_comments_route():
 
 if __name__ == "__main__":
     print("🎓  Canvas Grader running at http://localhost:5000")
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
